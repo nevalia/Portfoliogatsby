95a8666a5c52fe677f7e9ba8944cc5f65b47d596
nevalia <amarmaha2707@gmail.com>
2020-05-28 18:11:18 +0200

    nevalia
    add

---
+++ portfolio-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fed013e2-3997-41fe-9c90-443edcda92be'
+};
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,18 +1,17 @@
 import React from "react"
 import Title from "./Title"
-import { FaAngleDoubleRight, FaAlignRight } from "react-icons/fa"
+import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
-
-export const query = graphql`
+const query = graphql`
   {
-    
-    allStrapiJobs(sort: {fields: strapiId, order: DESC}) {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
       nodes {
+        strapiId
+        entreprise
         date
         position
-        entreprise
         desc {
           id
           name
@@ -21,37 +20,38 @@ export const query = graphql`
     }
   }
 `
-const Jobs = () => {
-  const data =  useStaticQuery(query);
-  const {allStrapiJobs : {nodes: jobs},
-} = data
 
+const Jobs = () => {
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
   const [value, setValue] = React.useState(0)
-  const {entreprise, position, date, desc} = jobs [value];
- 
+  const { entreprise, position, date, desc } = jobs[value]
+
   return (
-  <section className="section jobs">
-    <Title tile="experience"/>
-    <div className="jobs-center">
-      {/* btn container*/}
+    <section className="section jobs">
+      <Title title="expierence" />
+      <div className="jobs-center">
+        {/* btn container */}
         <div className="btn-container">
-          {jobs.map((item, index) =>{
-              return (<button 
-              key={item.strapiId}
-              onClick={()=> setValue(index)}
-               className={`job-btn &{index === value &&
-                 "active-btn"}`}>
-                   {item.entreprise}
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.strapiId}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.entreprise}
               </button>
-              )
+            )
           })}
         </div>
-
-       {/*job info*/}
-          <article className="job-info">
-            <h3>{position}</h3>
-            <h4>{entreprise}</h4>
-            <p className="job-date">{date}</p>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{entreprise}</h4>
+          <p className="job-date">{date}</p>
           {desc.map(item => {
             return (
               <div key={item.id} className="job-desc">
@@ -60,13 +60,13 @@ const Jobs = () => {
               </div>
             )
           })}
-          </article>
-    </div>
-            <Link to ="/about" className="btn center-btn">
-              more info
-            </Link>
-  </section>
-)
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        more info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
